78dc0de75eb32c0f8063672d856348dca0c9013f
sunqilin <852373728@qq.com>
2024-07-22 15:14:28 +0800

    1

--- cloud-consumer-order8000/src/main/java/com/qilin/cloudConsumerOrder.java
+++ cloud-consumer-order8000/src/main/java/com/qilin/cloudConsumerOrder.java
@@ -1,18 +1,17 @@
 package com.qilin;
 
+
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
-@MapperScan("com.qilin.mapper")
 @Slf4j
 @EnableDiscoveryClient
-public class CloudProviderPayment {
+public class cloudConsumerOrder {
     public static void main(String[] args) {
-        SpringApplication.run(CloudProviderPayment.class,args);
+        SpringApplication.run(cloudConsumerOrder.class,args);
 
         log.info("=========启动成功=========");
     }
